119f874c4f388704af4309bd518477b7fb310ba3
ninehnineh <103179810+ninehnineh@users.noreply.github.com>
2022-11-21 20:17:42 +0700

    first commit

---
+++ HR.LeaveManagement/HR.LeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Domain.Common;
+
+// make this Abstract class,
+// it cannot be instantialed by itseft
+public abstract class BaseDomainEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public string? ModifiedBy { get; set; }
+}
